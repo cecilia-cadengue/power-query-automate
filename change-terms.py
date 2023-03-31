8ac5e05c7c14ff9ffe3aff17bab6e524d1a92779
Cecilia Cadengue <123176898+cecilia-cadengue@users.noreply.github.com>
2023-03-31 12:32:38 -0300

    Update change-terms.py

--- change-terms.py
+++ change-terms.py
@@ -1,14 +1,15 @@
 # INPUTS NEEDED FOR POWER QUERY EDITOR CODE CHANGES
 sheet = input("Sheet name: ")
-client = input("Client: ")
+client = input("Engagement client name: ")
 name = input("Engagement Name: ")
 code = input("Engagement Code: ")
 partner = input("Engagement Partner: ")
+date = input("First meeting date: ")
 
 
 string = '''*PUT YOUR BASE POWER QUERY EDITOR CODE HERE BETWEEN TRIPLE SINGLE QUOTES TO KEEP FORMAT*'''
 
-string = string.replace("clnt", client).replace("eng nm", name).replace("eng cd", code).replace("eng prtn", partner).replace("sht", sheet)
+string = string.replace("clnt", client).replace("eng nm", name).replace("eng cd", code).replace("eng prtn", partner).replace("sht", sheet).replace("mt dt", date)
 
 # RESULTING POWER QUERY EDITOR CODE
 print(string)
